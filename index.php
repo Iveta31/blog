40039e9fa25de01d7965811795499cd6352d44cd
Iveta31 <36760673+Iveta31@users.noreply.github.com>
2018-02-26 14:00:37 +0100

    lunes1

--- index.php
+++ index.php
@@ -10,20 +10,42 @@
 <body>
     <div class="cabecera">
         <div class="contenidocabecera">
+            <a href="http:/www.ajeclm.com/" target="blank" class="logo"></a>
+            <a href="index.php"><h1>Blog del programa TIC</h1></a>
+                
             
         </div>
     </div>
     <div class="fondo">
         <div class="contenido">
+            <a href="https://www.jccm.es/" target="blank"> <img class="junta" src="imagenes/comunes/junta.jpg" alt="logo de la jccm"
+                title="logo de la jccm"/>
+                
+               </a>
+            <a href="http://www.ajeclm.com/programa-tic/" target="blank">
+                <img class="pragramatic" src="imagenes/comunes/programatic.jpg" alt="programatic" title="programatic"/>
+            </a>
+            
+            <a href="http://www.ajeclm.com/" target="blank"> <img class="aje" src="imagenes/comunes/aje,jpg"
+            alt="logo de aje" title="logo aje"/>
+            </a>
+            
+            
+             <a href="http://ec.europa.eu/esf/home.jsp?langId=es" target="blank"> <img class="europeo" src="imagenes/comunes/aje.jpg"
+            alt="logo del fondo social europeo" title="logo del fondo social europeo"/>
+            </a>
+        
+            
+            
             
         </div>
-        
-    </div>
     <div class="pie">
-        <div class="contenidopie"
-        
+            <div class="contenidopie">
+         </div>
     </div>
     
+    </div>
+   
     
 </body
 </html>
