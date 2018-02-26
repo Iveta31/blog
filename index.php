0ba4a1933c565964f084d2ebc1bfc9d4270b5c87
Iveta31 <36760673+Iveta31@users.noreply.github.com>
2018-02-26 10:01:54 +0100

    cambios del viernes

---
+++ index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <title>Blog</title>
+    <link href="estilos.css" rel="stylesheet" type="text/css" /> 
+</head>
+
+
+<body>
+    <div class="cabecera">
+        
+    </div>
+    <div class="cuerpo">
+        
+    </div>
+         
+    
+    
+</body
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+
+?>
